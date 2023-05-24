c2f65868802b920afa358dbcabb9df096d202d5a
hyejin <vvloveletter@naver.com>
2023-05-25 03:23:30 +0900

    0524 front

--- final-pjt/final-pjt-front/src/components/Movie/CurrentMovieReview.vue
+++ final-pjt/final-pjt-front/src/components/Movie/CurrentMovieReview.vue
@@ -1,12 +1,35 @@
 <!-- 리뷰 작성 폼 -->
 <template>
     <div>
-        <div v-for="(myReview) in moviereview" :key="myReview.id">
-            <p>별점 : {{ myReview.rate }}</p>
-            <p>{{ myReview.content }}</p>
-            <br>
-            <button class="btn" @click="deleteReview(myReview)">삭제</button>
+        <!-- 내가 쓴 리뷰 바로 띄우기 -->
+        <div class="review-box">
+            <div v-for="(myReview) in moviereview" :key="myReview.id">
+                <div class="star-ratings review-rate">
+                    <div 
+                    class="star-ratings-fill space-x-2 text-lg"
+                    :style="{ width: `${myReview.rate * 20}%` }"
+                    >
+                        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+                    </div>
+                    <div class="star-ratings-base space-x-2 text-lg">
+                        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+                    </div>
+                </div>
+                <div class="review-content">
+                    <div class="speech-bubble">{{ myReview.content }}</div>
+                    <div class="review-user">
+                        <router-link :to="{name:'userprofile', params:{userid:myReview.user}} "><i class="fa-solid fa-circle-user fa-2xl" style="color: #00a1b0"></i>
+                            <br><br>
+                        <span>{{ myReview.nickname }}</span></router-link>
+                        <br>
+                        <button class="btn" @click="deleteReview(myReview)"
+                        style="border:1px white solid; border-radius: 5px; height:30px; margin-left:5px; color:white">삭제</button>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        <!-- 리뷰 작성 -->
         <fieldset class="rate">
             <input type="radio" id="rating10" name="rating" value="10" @change="handleRatingChange"><label for="rating10" title="5점"></label>
             <input type="radio" id="rating9" name="rating" value="9" @change="handleRatingChange"><label class="half" for="rating9" title="4.5점"></label>
@@ -21,9 +44,13 @@
             <br>
             <hr>  
             <form @submit.prevent="createReview">
-                <label for="content">내용 : </label>
-                <textarea id="content" cols="30" rows="10" v-model="content"></textarea><br>
-                <input type="submit" id="submit">
+                <label for="content"></label>
+                <input id="content" cols="30" rows="10" v-model="content"
+                    style="color: white; width:500px; height: 50px; border: 1px solid white;
+                    border-radius: 5px; margin-top:10px; margin-bottom: 10px; text-align: center;"
+                    placeholder="리뷰를 입력하세요">
+                <input type="submit" id="submit" class="btn" value="확인"
+                style="border:1px white solid; border-radius: 5px; height:50px; margin-left:5px; color:white">
             </form>
         </fieldset>
     </div>
@@ -125,4 +152,83 @@ export default {
 .rate input input:checked ~ label:hover,
 .rate input:checked ~ .rate label:hover ~ label,  
 .rate label:hover ~ input:checked ~ label { color: #f73c32 !important;  } 
+
+
+.speech-bubble {
+    width: 400px;
+	position: relative;
+	background: #ffffff;
+	border-radius: .4em;
+    color: black;
+    margin-left: auto;
+}
+
+.speech-bubble:after {
+	content: '';
+	position: absolute;
+	right: 0;
+	top: 50%;
+	width: 0;
+	height: 0;
+	border: 0.875em solid transparent;
+	border-left-color: #ffffff;
+	border-right: 0;
+	border-top: 0;
+	margin-top: -0.437em;
+	margin-right: -0.875em;
+    margin-left: auto;
+}
+.review-box{
+    width: 80%;
+    margin-right: auto;
+    margin-left: auto;
+}
+.review-rate{
+    width: 80%;
+    margin-top: 30px;
+    margin-right: 500px;
+    margin-bottom: 5px;
+}
+.review-user{
+    align-self: auto;
+    margin-left: 20px;
+}
+.review-content{
+    display: flex;
+    justify-content: last baseline;
+}
+div a {
+  text-decoration: none;
+  text-decoration-line: none;
+  color: white;
+}
+.star-ratings {
+  color: #aaa9a9; 
+  position: relative;
+  unicode-bidi: bidi-override;
+  width: max-content;
+  -webkit-text-fill-color: transparent; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 1.3px;
+  -webkit-text-stroke-color: #2b2a29;
+}
+ 
+.star-ratings-fill {
+  color: #fff58c;
+  padding: 0;
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  -webkit-text-fill-color: gold;
+}
+ 
+.star-ratings-base {
+  z-index: 0;
+  padding: 0;
+}
+/* .review-container{
+    margin-left: auto;
+} */
 </style>
--- final-pjt/final-pjt-front/src/views/CurrentMovieDetail.vue
+++ final-pjt/final-pjt-front/src/views/CurrentMovieDetail.vue
@@ -1,45 +1,67 @@
 <template>
 <div class="currentMovieDetail">
-    <div >
-        <img :src="`https://www.themoviedb.org/t/p/original/${movie?.backdrop_path}`" alt="배경 이미지" class="backgroundimg">
-    </div>
-    <h1>{{movie?.title}}</h1>
-    <!-- 영화 포스터 및 타이틀 -->
-    <div>
-    <div>
-        <img :src="`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie?.poster_path}`" alt="포스터 이미지">
-    </div>
-    <div>
-        <div>
-        <span>줄거리 : {{ movie?.overview }}</span><br>
-        <span>개봉일 : {{ movie?.release_date }}</span><br>
-        장르 : <span v-for="(genre) in genres" :key="genre.id">{{ genre.name }}, </span>
+    <img :src="`https://www.themoviedb.org/t/p/original/${movie?.backdrop_path}`" alt="배경 이미지" class="backgroundimg">
+        <!-- 절대위치 주기 -->
+        <div class="movie-info">
+            <div class="col-6 col-md-4 info-img">
+                <img :src="`https://www.themoviedb.org/t/p/w300_and_h450_bestv2${movie?.poster_path}`" alt="포스터 이미지">
+            </div>
+            <div class="col-6 col-md-8 info">
+                <div class="movie-section">
+                    <div class="movie-title">
+                        <div style="text-align:left">
+                            <span v-for="(genre) in genres" :key="genre.id">#{{ genre.name }} </span>
+                        </div>
+                        <h1 style="text-align:left" class="my-2">{{movie?.title}}</h1>
+                        <p style="text-align:left">개봉 : {{ movie?.release_date }}</p>
+                    </div>
+                    <div class="heart">
+                        <i v-show="isLike && isMyLike" @click="likeMovie" class="fa-regular fa-heart fa-xl" style="color: #c11a33"></i>
+                        <i v-show="!isLike || !isMyLike" @click="likeMovie" class="fa-solid fa-heart fa-xl" style="color: #c11a33;"></i>
+                        <br>
+                        <span> {{ likeUserLength }}</span>
+                    </div>
+                </div>
+                    <!-- divider -->
+                    <div class="astrodivider">
+                        <div class="astrodividermask"></div>
+                    </div>
+                <br>
+                <div class="movie-overview">
+                    <p style="text-align:left">{{ movie?.overview }}</p>
+                </div>
+            </div>
         </div>
-        <div>
-            <!-- 영화 좋아요 -->
-            <button @click="likeMovie">
-                {{ (isLike || isMyLike) ? '좋아요' : '좋아요 취소'}} </button>
-            <br>
-            <span>좋아요 수 : {{ likeUserLength }}</span>
-        </div>
-    </div>
-    </div>
-    <!-- 영화 예고편 영상 -->
-    <div>
-    영화 예고편 유튜브
-    <iframe :src="videoURL" frameborder="0" width="500px" height="300px"></iframe>
-    </div>
+        <!-- 영화 예고편 영상 -->
+    <iframe :src="videoURL" frameborder="0" width="100%" height="800px" class="YTB"></iframe>
     <!-- 영화 리뷰 -->
     <div>
-    영화 리뷰
-    <p v-for="(review, idx) in reviews" :key="idx">
-        <span><router-link :to="{name:'userprofile', params:{userid:review.user}} ">작성자 : {{ review.nickname }}</router-link></span>
-        <span>별점 : {{ review.rate }}</span>
-        <br>
-        <span>{{ review.content }}</span>
-    </p>
-    
-    <CurrentMovieReview/>
+        <div class="review-box">
+            <div v-for="(review, idx) in reviews" :key="idx">
+                <div class="star-ratings review-rate">
+                    <div 
+                    class="star-ratings-fill space-x-2 text-lg"
+                    :style="{ width: `${review.rate * 20}%` }"
+                    >
+                        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+                    </div>
+                    <div class="star-ratings-base space-x-2 text-lg">
+                        <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
+                    </div>
+                </div>
+                <div class="review-content">
+                    <div class="review-user">
+                        <router-link :to="{name:'userprofile', params:{userid:review.user}} "><i class="fa-solid fa-circle-user fa-2xl" style="color: #00a1b0"></i>
+                            <br><br>
+                        <span>{{ review.nickname }}</span></router-link>
+                    </div>
+                    
+                    <div class="speech-bubble">{{ review.content }}</div>
+                </div>
+            </div>
+        </div>
+        
+        <CurrentMovieReview/>
     </div>
 </div>
 </template>
@@ -47,9 +69,10 @@
 <script>
 import CurrentMovieReview from '../components/Movie/CurrentMovieReview.vue'
 import axios from 'axios'
+
 const API_URL = 'http://127.0.0.1:8000/api/v1'
-// const YOUTUBE_KEY = 'AIzaSyBkdQF46yaw0HH0kpENBBanwFI1y1mGnhI'
-// const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
+const YOUTUBE_KEY = 'AIzaSyBkdQF46yaw0HH0kpENBBanwFI1y1mGnhI'
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 export default {
 name : 'CurrentMovieDetail',
@@ -85,6 +108,7 @@ methods:{
             // this.getMovieTrailer() //유튜브
             this.likeUserLength = res.data.like_users.length
             this.genres = res.data.genres
+
             const user_pk = this.$store.state.user.pk
             if (this.movie.like_users.includes(user_pk)){
                 this.isMyLike = false
@@ -99,28 +123,26 @@ methods:{
             console.log('getMovieDetail err', err)
         })
     },
-    // getMovieTrailer(){
-    //     const params = {
-    //         key : YOUTUBE_KEY,
-    //         part : 'snippet',
-    //         type:'video',
-    //         q : this.title
-    //     }
-    //     axios({
-    //         method:'get',
-    //         url:YOUTUBE_URL,
-    //         params,
-    //     })
-    //     .then(res=>{
-    //         console.log(res)
-    //         this.MovieTrailer = res.data.items[0]
-    //         console.log(this.MovieTrailer)
-    //         this.videoURL = `https://youtube.com/embed/${this.MovieTrailer.id.videoId}`
-    //     })
-    //     .catch(err=>{
-    //         console.log(err)
-    //     })
-    // },
+    getMovieTrailer(){
+        const params = {
+            key : YOUTUBE_KEY,
+            part : 'snippet',
+            type:'video',
+            q : this.title
+        }
+        axios({
+            method:'get',
+            url:YOUTUBE_URL,
+            params,
+        })
+        .then(res=>{
+            this.MovieTrailer = res.data.items[0]
+            this.videoURL = `https://youtube.com/embed/${this.MovieTrailer.id.videoId}`
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    },
     likeMovie(){
         const token = this.$store.state.login.token
         axios({
@@ -143,7 +165,8 @@ methods:{
             console.log(err)
         })
     },
-}
+    },
+
 }
 </script>
 
@@ -151,11 +174,148 @@ methods:{
 .currentMovieDetail{
     color: white;
     margin-top: 50px;
+    width: 90%;
+    margin: auto;
 }
 .backgroundimg{
     width: 100%;
     height: 40%;
     opacity: 0.3;
     filter: blur(3px);
+    border-radius: 10px;
+    margin-top: 50px;
+}
+.movie-info{
+    height: 350px;
+    display: flex;
+    justify-content: space-around;
+    position: static;
+    top: 90%;
+    transform: translateY(-50%);
+}
+.info-img{
+    padding: 5px;
+    width: 350px;
+
+}
+.info{
+    padding: 10px;
+    /* margin : 5px; */
+}
+.movie-section{
+    display: flex;
+    justify-content: space-between;
+}
+.movie-overview{
+    margin-bottom: auto;
+}
+.movie-title{
+    margin-right:auto;
+}
+.heart{
+    align-self: center;
+    margin-right: 20px;
+}
+.astrodivider {
+  margin:auto;
+  width:100%; 
+  max-width: 100%;
+  position:relative;
+}
+
+.astrodividermask { 
+    overflow:hidden; height:20px; 
+}
+
+.astrodividermask:after { 
+      content:''; 
+      display:block; margin:-25px auto 0;
+      width:100%; height:25px;  
+        border-radius:125px / 12px;
+       box-shadow:0 0 8px #4DD0E1;
+}
+
+.astrodivider i {
+    position:absolute;
+    top:4px; bottom:4px;
+    left:4px; right:4px;
+    border-radius:100%;
+    border:1px dashed #68beaa;
+    text-align:center;
+    line-height:40px;
+    font-style:normal;
+     color:#4DD0E1;
+}
+.YTB{
+    margin-top: 0px;
+}
+.speech-bubble {
+    width: 400px;
+	position: relative;
+	background:#00a1b0;
+	border-radius: .4em;
+}
+.speech-bubble:after {
+	content: '';
+	position: absolute;
+	left: 0;
+	top: 50%;
+	width: 0;
+	height: 0;
+	border: 0.875em solid transparent;
+	border-right-color: #00a1b0;
+	border-left: 0;
+	border-top: 0;
+	margin-top: -0.437em;
+	margin-left: -0.875em;
+}
+.review-box{
+    width: 80%;
+    margin-right: auto;
+    margin-left: auto;
+}
+.review-rate{
+    margin-top: 30px;
+    margin-left : 370px;
+    margin-bottom: 5px;
+}
+.review-user{
+    align-self: auto;
+    margin-right: 25px;
+}
+.review-content{
+    display: flex;
+    justify-content: first baseline;
+}
+div a {
+  text-decoration: none;
+  text-decoration-line: none;
+  color: white;
+}
+.star-ratings {
+  color: #aaa9a9; 
+  position: relative;
+  unicode-bidi: bidi-override;
+  width: max-content;
+  -webkit-text-fill-color: transparent; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 1.3px;
+  -webkit-text-stroke-color: #2b2a29;
+}
+ 
+.star-ratings-fill {
+  color: #fff58c;
+  padding: 0;
+  position: absolute;
+  z-index: 1;
+  display: flex;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  -webkit-text-fill-color: gold;
+}
+ 
+.star-ratings-base {
+  z-index: 0;
+  padding: 0;
 }
 </style>
